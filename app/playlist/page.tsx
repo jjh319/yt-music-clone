105ac8d418bdfae5e4fcfe5e90f13fdb218f7a5d
jjh319 <136777270+jjh319@users.noreply.github.com>
2024-05-10 17:43:23 +0900

    explore - carousel 추가

--- app/playlist/page.tsx
+++ app/playlist/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const page = (props) => {
-    return <div>playlist {props.searchParams.list}</div>;
+const page = (props: any) => {
+   return <div>playlist {props.searchParams.list}</div>;
 };
 
 export default page;
