9938cdcb77e842c433bdd2096ad623acf7d469a8
jjh319 <136777270+jjh319@users.noreply.github.com>
2024-05-10 15:01:49 +0900

    썸네일 버튼 추가

--- components/PlayListCard.jsx
+++ components/PlayListCard.jsx
@@ -3,6 +3,9 @@ import Image from 'next/image';
 import React from 'react';
 import { getRandomElementFromArray } from '@/lib/utils';
 import { useRouter } from 'next/navigation';
+import { MdMoreVert } from 'react-icons/md';
+import { FiPlay } from 'react-icons/fi';
+import IconButton from './elements/IconButton';
 
 const PlayListCard = ({ playlist }) => {
    const { push } = useRouter();
@@ -16,10 +19,24 @@ const PlayListCard = ({ playlist }) => {
       push(`/playlist?list=${id}`);
    };
 
+   const onClickPlay = () => {};
+
    return (
-      <article className="h-[240px] cursor-pointer">
+      <article className="h-[240px] cursor-pointer group">
          <section onClick={onClickCard} className="relative h-[136px]">
             <Image src={imgSrc} fill={true} alt="thumbnail" className="object-cover" />
+            <div className="hidden relative group-hover:block bg-gradient-to-b from-[rgba(0,0,0,0.7)] top-0 w-full h-[136px]">
+               <div className="absolute top-2 right-4">
+                  <IconButton icon={<MdMoreVert size={20} />} />
+               </div>
+               <div
+                  onClick={onClickPlay}
+                  className="absolute bottom-4 right-4 flex items-center justify-center transform-gpu transition-transform hover:scale-110
+                     bg-[rgba(0,0,0,0.7)] w-[45px] h-[45px] rounded-full hover:bg-[rgba(0,0,0,0.9)]
+               ">
+                  <FiPlay size={20} />
+               </div>
+            </div>
          </section>
          <section className="mt-2">
             <div>{playlistName}</div>
