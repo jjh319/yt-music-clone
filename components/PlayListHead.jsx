0d4e9fef16d3ffbaf3b7a9ef6b9756e88caa959c
jjh319 <136777270+jjh319@users.noreply.github.com>
2024-05-14 12:44:41 +0900

    playlist - Header 반응형 UI 작업 및 버튼 추가

--- components/PlayListHead.jsx
+++ components/PlayListHead.jsx
@@ -1,9 +1,11 @@
 'use client';
 import React from 'react';
 import IconButton from './elements/IconButton';
-import { FiMoreVertical } from 'react-icons/fi';
+import { FiFolderPlus, FiMoreVertical, FiPlay } from 'react-icons/fi';
 import Image from 'next/image';
 import { getRandomElementFromArray } from '@/lib/utils';
+import DarkButton from './elements/DarkButton';
+import WhiteButton from './elements/WhiteButton';
 
 const PlayListHead = ({ playList = {} } = {}) => {
    const { playlistName, owner, songList } = playList;
@@ -11,20 +13,29 @@ const PlayListHead = ({ playList = {} } = {}) => {
    const randomSong = getRandomElementFromArray(songList);
 
    return (
-      <section className="flex gap-[50px] flex-row">
-         <div className="relative h-[160px] w-[160px] lg:h-[240px] lg:w-[240px]">
-            <Image alt="songImg" fill src={randomSong?.imageSrc} />
-         </div>
-         <article className="flex flex-col justify-center">
-            <div className="font-bold text-[28px]">{playlistName}</div>
-            <div className="text-neutral-400 mt-4 text-[14px]">
-               <div>{`앨범 • ${owner} • 2019`}</div>
-               <div>{`${songList.length}곡 • 15분`}</div>
+      <section>
+         <div className="flex gap-[50px] flex-row">
+            <div className="relative h-[160px] w-[160px] lg:h-[240px] lg:w-[240px]">
+               <Image alt="songImg" fill src={randomSong?.imageSrc} />
             </div>
-            <ul>
-               <IconButton icon={<FiMoreVertical size={24} />} />
-            </ul>
-         </article>
+            <article className="flex flex-col justify-center">
+               <div className="font-bold text-[28px]">{playlistName}</div>
+               <div className="text-neutral-400 mt-4 text-[14px]">
+                  <div>{`앨범 • ${owner} • 2019`}</div>
+                  <div>{`${songList.length}곡 • 15분`}</div>
+               </div>
+               <ul className="hidden lg:flex flex-row gap-4 mt-7">
+                  <WhiteButton className={'w-[85px]'} icon={<FiPlay />} label="재생" />
+                  <DarkButton className={'w-[135px] text-[13px]'} icon={<FiFolderPlus />} label={'보관함에 저장'} />
+                  <IconButton icon={<FiMoreVertical size={24} />} />
+               </ul>
+            </article>
+         </div>
+         <ul className="flex flex-row gap-4 mt-7 lg:hidden">
+            <WhiteButton className={'w-[85px]'} icon={<FiPlay />} label="재생" />
+            <DarkButton className={'w-[135px] text-[13px]'} icon={<FiFolderPlus />} label={'보관함에 저장'} />
+            <IconButton icon={<FiMoreVertical size={24} />} />
+         </ul>
       </section>
    );
 };
