a0a1b7f19ea27ef7780d4d2b0b4192fd9fdb0e9c
jjh319 <136777270+jjh319@users.noreply.github.com>
2024-05-16 01:21:16 +0900

    노래 재생 - 이전, 다음 기능 추가 및 autoNextPlay 추가

--- components/player/PlayerContent.jsx
+++ components/player/PlayerContent.jsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { Slider as PlayerSlider } from '../ui/playerSlider';
 import { useAudio } from 'react-use';
 import { IoPlaySkipBackSharp, IoPlaySkipForwardSharp, IoShuffle, IoVolumeHighOutline } from 'react-icons/io5';
@@ -11,22 +11,50 @@ import Image from 'next/image';
 import { RxLoop } from 'react-icons/rx';
 
 const PlayerContent = () => {
-   const { activeSong } = usePlayerState();
+   const { activeSong, nextPlayerQueue, prevPlayerQueue, playBack, playNext } = usePlayerState();
    const [audio, state, controls, ref] = useAudio({
-      src: activeSong.src,
-      autoPlay: false,
+      src: activeSong?.src,
+      autoPlay: true,
    });
 
    const isLoading = activeSong?.src && state.buffered?.length === 0;
 
-   const onClickPreBtn = () => {};
-   const onClickStartBtn = () => {
-      controls.play();
+   const onClickPrevBtn = () => {
+      if (state.playing && state.time > 10) {
+         controls.seek(0);
+         return;
+      }
+
+      if (prevPlayerQueue.length === 0) return;
+      playBack();
    };
+
+   const onClickStartBtn = () => {
+      if (activeSong) {
+         controls.play();
+      } else {
+         playNext();
+      }
+   };
+
    const onClickPauseBtn = () => {
       controls.pause();
    };
-   const onClickNextBtn = () => {};
+
+   const onClickNextBtn = useCallback(() => {
+      if (nextPlayerQueue.length === 0) {
+         controls.pause();
+      } else {
+         playNext();
+      }
+   }, [controls, playNext, nextPlayerQueue]);
+
+   useEffect(() => {
+      ref?.current?.addEventListener('ended', onClickNextBtn);
+      return () => {
+         ref?.current?.removeEventListener('ended', onClickNextBtn);
+      };
+   }, [ref, onClickNextBtn]);
 
    return (
       <div className="h-full w-full relative">
@@ -38,12 +66,13 @@ const PlayerContent = () => {
                onValueChange={value => {
                   controls.seek(value);
                }}
+               max={state.duration}
             />
          </div>
          {audio}
          <section className="flex flex-row justify-between items-center w-full h-full px-2 lg:px-6">
             <div className="flex flex-row h-full items-center gap-1 lg:gap-8">
-               <IoPlaySkipBackSharp size={24} className="cursor-pointer" onClick={onClickPreBtn} />
+               <IoPlaySkipBackSharp size={24} className="cursor-pointer" onClick={onClickPrevBtn} />
                {isLoading ? (
                   <ClipLoader color="#FFF" />
                ) : state.playing ? (
@@ -57,7 +86,7 @@ const PlayerContent = () => {
             <article>
                <div className="flex flex-row gap-4 items-center">
                   <div className="relative w-[40px] h-[40px]">
-                     <Image fill className="object-cover" src={activeSong.imageSrc} alt="img" />
+                     <Image fill className="object-cover" src={activeSong?.imageSrc} alt="img" />
                   </div>
                   <div className="flex flex-col">
                      <div>{activeSong?.name}</div>
