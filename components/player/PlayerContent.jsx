4c8a6f2e3eee994917bc57e5214ffe725afcc51b
jjh319 <136777270+jjh319@users.noreply.github.com>
2024-05-15 16:53:01 +0900

    Slider 노래 재생 연동 - 1

--- components/player/PlayerContent.jsx
+++ components/player/PlayerContent.jsx
@@ -1,29 +1,53 @@
+'use client';
 import React from 'react';
 import { Slider as PlayerSlider } from '../ui/playerSlider';
 import { useAudio } from 'react-use';
+import { IoPlaySkipBackSharp, IoPlaySkipForwardSharp } from 'react-icons/io5';
+import { AiOutlinePause } from 'react-icons/ai';
+import usePlayerState from '@/hooks/usePlayerState';
+import { ClipLoader } from 'react-spinners';
+import { RiPlayFill } from 'react-icons/ri';
 
 const PlayerContent = () => {
+   const { activeSong } = usePlayerState();
    const [audio, state, controls, ref] = useAudio({
-      src: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',
+      src: activeSong.src,
       autoPlay: true,
    });
 
+   const isLoading = activeSong?.src && state.buffered?.length === 0;
+
+   const onClickPreBtn = () => {};
+   const onClickStartBtn = () => {
+      controls.play();
+   };
+   const onClickPauseBtn = () => {
+      controls.pause();
+   };
+   const onClickNextBtn = () => {};
+
    return (
-      <div>
+      <div className="h-full w-full relative">
+         <div className="absolute top-[-16px] w-full">
+            <PlayerSlider />
+         </div>
          {audio}
-         <pre>{JSON.stringify(state, null, 2)}</pre>
-         <button onClick={controls.pause}>Pause</button>
-         <button onClick={controls.play}>Play</button>
-         <br />
-         <button onClick={controls.mute}>Mute</button>
-         <button onClick={controls.unmute}>Un-mute</button>
-         <br />
-         <button onClick={() => controls.volume(0.1)}>Volume: 10%</button>
-         <button onClick={() => controls.volume(0.5)}>Volume: 50%</button>
-         <button onClick={() => controls.volume(1)}>Volume: 100%</button>
-         <br />
-         <button onClick={() => controls.seek(state.time - 5)}>-5 sec</button>
-         <button onClick={() => controls.seek(state.time + 5)}>+5 sec</button>
+         <section className="flex flex-row justify-between items-center w-full h-full px-2 lg:px-6">
+            <div className="flex flex-row h-full items-center gap-1 lg:gap-8">
+               <IoPlaySkipBackSharp size={24} className="cursor-pointer" onClick={onClickPreBtn} />
+               {isLoading ? (
+                  <ClipLoader color="#FFF" />
+               ) : state.playing ? (
+                  <AiOutlinePause size={40} className="cursor-pointer" onClick={onClickPauseBtn} />
+               ) : (
+                  <RiPlayFill size={40} className="cursor-pointer" onClick={onClickStartBtn} />
+               )}
+
+               <IoPlaySkipForwardSharp size={24} className="cursor-pointer" onClick={onClickNextBtn} />
+            </div>
+            <article></article>
+            <div></div>
+         </section>
       </div>
    );
 };
